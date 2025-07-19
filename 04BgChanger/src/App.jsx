9e45f2d2bb9c6b15297f9884279438fabb97be51
khanajmal8055 <mdajamal3364@gmail.com>
2025-07-19 12:19:00 +0530

    Context API

--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("red")
+  const [color, setColor] = useState("olive")
   console.log(color);
   
 
